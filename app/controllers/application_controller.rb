3dce0b20166fe99c5bc4889cc64ceca572fc09e6
tabuchi0919 <tabuchi.igo.0919@gmail.com>
2021-08-28 18:55:21 +0900

    refactor: move controller implement to super class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,63 @@ class ApplicationController < ActionController::Base
   ADMIN_EDITABLE_CLASSES = [
     User
   ].freeze
+
+  def index
+    @resources = model.all
+    render 'shared/index'
+  end
+
+  def new
+    @resource = model.new
+    render 'shared/form'
+  end
+
+  def create
+    @resource = model.new(permitted_params)
+    if @resource.save
+      redirect_to model, notice: t('flash.success.create')
+    else
+      flash.now[:alert] = t('flash.failure.create')
+      flash.now[:error] = @resource.errors.full_messages
+      render 'shared/form'
+    end
+  end
+
+  def edit
+    @resource = model.find(params[:id])
+    render 'shared/form'
+  end
+
+  def update
+    @resource = model.find(params[:id])
+    if @resource.update(permitted_params)
+      redirect_to model, notice: t('flash.success.update')
+    else
+      flash.now[:alert] = t('flash.failure.update')
+      flash.now[:error] = @resource.errors.full_messages
+      render 'shared/form'
+    end
+  end
+
+  def destroy
+    @resource = model.find(params[:id])
+    if @resource.destroy
+      redirect_to model, notice: t('flash.success.destroy')
+    else
+      flash.now[:error] = @resource.errors.full_messages
+      redirect_to model, alert: t('flash.failure.destroy')
+    end
+  end
+
+  private
+
+  def permitted_params
+    params.require(model.model_name.singular).permit(
+      model.editable_columns.map(&:name)
+    )
+  end
+
+  def model
+    raise NotInplementedError
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,58 +1,9 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def index
-    @resources = User.all
-    render 'shared/index'
-  end
-
-  def new
-    @resource = User.new
-    render 'shared/form'
-  end
-
-  def create
-    @resource = User.new(permitted_params)
-    if @resource.save
-      redirect_to User, notice: t('flash.success.create')
-    else
-      flash.now[:alert] = t('flash.failure.create')
-      flash.now[:error] = @resource.errors.full_messages
-      render 'shared/form'
-    end
-  end
-
-  def edit
-    @resource = User.find(params[:id])
-    render 'shared/form'
-  end
-
-  def update
-    @resource = User.find(params[:id])
-    if @resource.update(permitted_params)
-      redirect_to User, notice: t('flash.success.update')
-    else
-      flash.now[:alert] = t('flash.failure.update')
-      flash.now[:error] = @resource.errors.full_messages
-      render 'shared/form'
-    end
-  end
-
-  def destroy
-    @resource = User.find(params[:id])
-    if @resource.destroy
-      redirect_to User, notice: t('flash.success.destroy')
-    else
-      flash.now[:error] = @resource.errors.full_messages
-      redirect_to User, alert: t('flash.failure.destroy')
-    end
-  end
-
   private
 
-  def permitted_params
-    params.require(:user).permit(
-      User.editable_columns.map(&:name)
-    )
+  def model
+    User
   end
 end
