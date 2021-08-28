348fccdd4fb77ee0fbd26be22d461db048d6a124
tabuchi0919 <tabuchi.igo.0919@gmail.com>
2021-08-28 19:04:57 +0900

    feat: add seed for posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 
-(1..10).each { |i| User.find_or_create_by!(name: "User#{i}") }
+(1..10).each do |i|
+  user = User.find_or_create_by!(name: "User#{i}")
+  (1..2).each do |j|
+    user.posts.find_or_create_by!(body: "Post#{j} by #{user.name}")
+  end
+end
+
